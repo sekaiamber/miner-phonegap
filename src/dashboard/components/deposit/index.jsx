f36373db41c8cf914ab64779cb6d5880f998d339
xuxiaomeng <xuxiaomeng@baozou.com>
2019-05-20 10:56:08 +0800

    feature. update.

--- src/dashboard/components/deposit/index.jsx
+++ src/dashboard/components/deposit/index.jsx
@@ -11,6 +11,7 @@ import saveImage from '../../../utils/saveImage';
 import './style.scss';
 
 import walletBaseImg from '../../../assets/wallet_base.svg';
+import bitrabbitImg from '../../../assets/bitrabbit.svg';
 
 // images
 class Deposit extends Component {
@@ -27,15 +28,20 @@ class Deposit extends Component {
   handleSaveImage = () => {
     const { url } = this.state;
     saveImage(url, () => {
-      message.success('成功');
+      message.success('已成功保存到相冊');
     }, () => {
-      message.error('失敗');
+      message.error('自動保存失敗，請手動截屏');
     });
+  }
+
+  handleOpenBitrabbit = () => {
+    if (window.cordova) {
+      window.cordova.InAppBrowser.open('https://bitrabbit.io', '_system', 'location=yes');
+    }
   }
 
   render() {
     const { userInfo } = this.props;
-    console.log(userInfo);
 
     return (
       <div id="deposit" className="container">
@@ -48,6 +54,12 @@ class Deposit extends Component {
             <div className="qrcode"><Qrcode data={userInfo.payment_address} option={{ height: 250, width: 250, margin: 2 }} onUrlChange={this.handleUrlChange} /></div>
           )}
           <div className="btn" onClick={this.handleSaveImage}>保存二維碼</div>
+          <div className="desc">BASE 充值地址</div>
+          <div className="address clipboard-target" data-clipboard-text={userInfo.payment_address}>{userInfo.payment_address}</div>
+        </div>
+        <div className="bitrabbit">BASE 購買渠道</div>
+        <div className="btn" onClick={this.handleOpenBitrabbit}>
+          <img src={bitrabbitImg} alt="" />
         </div>
       </div>
     );
