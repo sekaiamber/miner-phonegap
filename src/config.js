82d952e004bf7572807fc206bee1b5af18d0ff42
xuxiaomeng <xuxiaomeng@baozou.com>
2019-06-20 16:14:37 +0800

    feature. udpate.

--- src/config.js
+++ src/config.js
@@ -1,9 +1,3 @@
-// const CONFIG = __CONFIG__;
-const CONFIG = {
-  "DOMAIN": "https://www.basepool.net",
-  "KEY": "fc44c6d77c2c8677c54e",
-  "SECRET": "05ab3f036cf1f4bd6914edf379bb4ecb"
-}
-
+const CONFIG = __CONFIG__;
 
 export default CONFIG;
