78cdec5b036c99b81fe7ed87f7df1cd03c36a9f9
xuxiaomeng <xuxiaomeng@baozou.com>
2019-07-19 10:19:42 +0800

    feature. update.

--- src/config.js
+++ src/config.js
@@ -1,9 +1,3 @@
-// const CONFIG = __CONFIG__;
-const CONFIG = {
-  "DOMAIN": "https://www.basepool.net",
-  "KEY": "cee670f8a791feecfb0c",
-  "SECRET": "b5a1186f1e29b2d77b59103480c99796"
-}
-
+const CONFIG = __CONFIG__;
 
 export default CONFIG;
